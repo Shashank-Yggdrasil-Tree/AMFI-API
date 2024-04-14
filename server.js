284f51c5e4cf9ad74947d83549d7699809ebdaf5
Shashank-Yggdrasil-Tree <shashankdwivedi1222000@gmail.com>
2024-04-14 20:27:51 +0530

    added rate limit

--- server.js
+++ server.js
@@ -19,9 +19,4 @@ const limiter = rateLimit({
 
 app.use('/', limiter)
 
-var server = app.listen(process.env.PORT || 8080, function () {
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port)
-})
+app.listen(process.env.PORT || 8080)
