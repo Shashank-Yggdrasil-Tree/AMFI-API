790bf7f2707b37d0c13f7354f3f33add1e5d34cb
Shashank-Yggdrasil-Tree <shashankdwivedi1222000@gmail.com>
2024-04-14 20:26:17 +0530

    added rate limit

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require('express')
+import rateLimit from 'express-rate-limit'
 var app = express()
 
-var api = require('./api')
+var api = require('./index')
 
 app.use('/', api)
+app.use(express.json())
+
+// GLOBAL MIDDLEWARE
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  handler: (req, res) => {
+      res.status(429).send('Too many request, please try again in an hour')
+  },
+})
+
+app.use('/', limiter)
 
 var server = app.listen(process.env.PORT || 8080, function () {
   var host = server.address().address
